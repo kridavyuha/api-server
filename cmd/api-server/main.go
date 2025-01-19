3a7857afb9608c4dd0419c6f3100f97fb26fbeeb
Rithvik89 <rithvikalkanti@gmail.com>
2025-01-19 18:19:10 +0530

    Implement RabbitMQ integration and transaction queue initialization

--- cmd/api-server/main.go
+++ cmd/api-server/main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	KVStore "backend/pkg"
+	"log"
 
 	"net/http"
 	"sync"
+
+	amqp "github.com/rabbitmq/amqp091-go"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/gorilla/websocket"
@@ -18,12 +21,22 @@ type App struct {
 	WS       map[*websocket.Conn]WSDetails
 	ClientsM sync.Mutex
 	KVStore  KVStore.KVStore
+	Ch       *amqp.Channel
 }
 
 func main() {
 
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	failOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	defer ch.Close()
+
 	app := &App{
 		WS: make(map[*websocket.Conn]WSDetails),
+		Ch: ch,
 	}
 
 	db, err := app.initDB()
@@ -46,9 +59,57 @@ func main() {
 	app.R = r
 
 	// create a map relation btw  player name and player_id
-
+	// app.initQueue()
 	app.initHandlers()
 	app.initKVStore()
+	app.initTxnQueue()
+
+	err = ch.ExchangeDeclare(
+		"balls",  // name
+		"fanout", // type
+		true,     // durable
+		false,    // auto-deleted
+		false,    // internal
+		false,    // no-wait
+		nil,      // arguments
+	)
+	failOnError(err, "Failed to declare an exchange")
+
+	q, err := ch.QueueDeclare(
+		"",    // name
+		false, // durable
+		false, // delete when unused
+		true,  // exclusive
+		false, // no-wait
+		nil,   // arguments
+	)
+	failOnError(err, "Failed to declare a queue")
+
+	err = ch.QueueBind(
+		q.Name,  // queue name
+		"",      // routing key
+		"balls", // exchange
+		false,
+		nil,
+	)
+	failOnError(err, "Failed to bind a queue")
+	msgs, err := ch.Consume(
+		q.Name, // queue
+		"",     // consumer
+		true,   // auto-ack
+		false,  // exclusive
+		false,  // no-local
+		false,  // no-wait
+		nil,    // args
+	)
+	failOnError(err, "Failed to register a consumer")
+
+	go func() {
+		for d := range msgs {
+			log.Printf(" [x] %s", d.Body)
+			app.BallPicker(d.Body)
+		}
+	}()
 
 	if err := http.ListenAndServe(":8080", r); err != nil {
 		panic(err)
