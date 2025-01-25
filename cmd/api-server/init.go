87ea1071f648b496e87810ff10a55a14ab27ff78
Rithvik89 <rithvikalkanti@gmail.com>
2025-01-25 18:36:11 +0530

    Refactor KVStore initialization and add consumer setup in api-server

--- cmd/api-server/init.go
+++ cmd/api-server/init.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 
 	"github.com/kridavyuha/api-server/pkg/kvstore"
@@ -25,19 +26,67 @@ func (app *App) initDB() (*gorm.DB, error) {
 	return db, nil
 }
 
-func (app *App) initKVStore() {
+func (app *App) initKVStore() error {
 	// initialize redis
-	app.KVStore = kvstore.NewRedis("localhost:6379", "", 0)
+	kvstore, err := kvstore.NewRedis("localhost:6379", "", 0)
+	if err != nil {
+		fmt.Println("Error initializing KVStore: ", err)
+		return err
+	}
+	app.KVStore = kvstore
+	return nil
 }
 
-func (app *App) initTxnQueue() {
-	_, err := app.Ch.QueueDeclare(
-		"txns", // name
-		false,  // durable
-		false,  // delete when unused
-		false,  // exclusive
-		false,  // no-wait
-		nil,    // arguments
+func (app *App) initConsumer() {
+
+	fmt.Println("Initializing consumer")
+	ch, err := app.MQConn.Channel()
+	failOnError(err, "Failed to open a channel")
+	err = ch.ExchangeDeclare(
+		"balls",  // name
+		"fanout", // type
+		true,     // durable
+		false,    // auto-deleted
+		false,    // internal
+		false,    // no-wait
+		nil,      // arguments
+	)
+	failOnError(err, "Failed to declare an exchange")
+
+	q, err := ch.QueueDeclare(
+		"",    // name
+		false, // durable
+		false, // delete when unused
+		true,  // exclusive
+		false, // no-wait
+		nil,   // arguments
 	)
 	failOnError(err, "Failed to declare a queue")
+
+	err = ch.QueueBind(
+		q.Name,  // queue name
+		"",      // routing key
+		"balls", // exchange
+		false,
+		nil,
+	)
+	failOnError(err, "Failed to bind a queue")
+	msgs, err := ch.Consume(
+		q.Name, // queue
+		"",     // consumer
+		true,   // auto-ack
+		false,  // exclusive
+		false,  // no-local
+		false,  // no-wait
+		nil,    // args
+	)
+	failOnError(err, "Failed to register a consumer")
+
+	go func() {
+		for d := range msgs {
+			log.Printf(" [x] %s", d.Body)
+			app.BallPicker(d.Body)
+		}
+	}()
+
 }
