bc4d574a66fabfaa6c826775d11dab4eb5718202
Rithvik89 <rithvikalkanti@gmail.com>
2025-01-27 18:30:59 +0530

    Add CacheService for managing player data and user balances with Redis caching

---
+++ internals/cache/model.go
@@ -0,0 +1,20 @@
+package cache
+
+import "time"
+
+type PlayerInLeague struct {
+	PlayerID    string    `json:"player_id"`
+	CurPrice    float64   `json:"cur_price"`
+	BasePrice   float64   `json:"base_price"`
+	LastUpdated time.Time `json:"last_updated"`
+	Status      string    `json:"status"`
+}
+
+type Portfolio struct {
+	PlayerId   string  `json:"player_id"`
+	Shares     int     `json:"shares"`
+	AvgPrice   float64 `json:"avg_price"`
+	CurPrice   float64 `json:"cur_price"`
+	PlayerName string  `json:"player_name"`
+	TeamName   string  `json:"team_name"`
+}
