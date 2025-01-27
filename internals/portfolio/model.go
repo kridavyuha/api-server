edbe4088da6ea1e30c41e786e7fec0248f2dac6b
Rithvik89 <rithvikalkanti@gmail.com>
2025-01-27 18:31:06 +0530

    Refactor Portfolio model and service to use float64 for price and balance fields

--- internals/portfolio/model.go
+++ internals/portfolio/model.go
@@ -1,15 +1,15 @@
 package portfolio
 
 type Portfolio struct {
-	PlayerId   string `json:"player_id"`
-	Shares     int    `json:"shares"`
-	Invested   int    `json:"invested"`
-	CurPrice   int    `json:"cur_price"`
-	PlayerName string `json:"player_name"`
-	TeamName   string `json:"team_name"`
+	PlayerId   string  `json:"player_id"`
+	Shares     int     `json:"shares"`
+	AvgPrice   float64 `json:"avg_price"`
+	CurPrice   float64 `json:"cur_price"`
+	PlayerName string  `json:"player_name"`
+	TeamName   string  `json:"team_name"`
 }
 
 type DetailedPortfolio struct {
 	Players []Portfolio `json:"players"`
-	Balance int         `json:"balance"`
+	Balance float64     `json:"balance"`
 }
