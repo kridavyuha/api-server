11b7de307f9efb97d83c3f4e3017e3832a0249cc
Sai Rithvik Reddy Alkanti <rithvikalkanti@gmail.com>
2025-02-21 00:09:40 +0530

    Bug (#52)
    * Add ActivateLeague endpoint and implement league activation logic
    
    * Fix key pattern in GetPlayerDetails to correctly match player keys for the specified league
    
    * Add error handling for Redis cache key setting in LoadUserPortfolioData and clean up imports in profile package
    
    * Refactor BallPicker to use PerfDetails and CoreDetails structs for websocket updates; implement HandleUpdateCorePrices for core price updates from Redis cache
    
    * Implement core processing initialization to fetch league statuses and update core prices every 2 seconds
    
    * Update portfolio schema to include player_id in primary key; add notification and transaction tables
    
    * Add feedback package and notification models for handling notifications and transactions
    
    * Add NotificationService for managing user notifications and updating their status
    
    * Add notification handlers for retrieving and updating user notifications
    
    * Fix player key retrieval pattern in TradeService to include underscore separator
    
    * Add GetActivePortfolios method to PortfolioService for retrieving active leagues
    
    * Add error logging for session token insertion and fix notification query to use transaction ID
    
    * Add CreatedAt field to Notification model and update notification retrieval logic

--- internals/notification/notification.go
+++ internals/notification/notification.go
@@ -44,15 +44,16 @@ func (ns *NotificationService) GetNotifications(user_id int) ([]Notification, er
 
 	for _, notif := range details {
 		notification := Notification{
-			Actor:  1,
-			Status: notif.Status,
-			Id:     notif.Id,
+			Actor:     1,
+			Status:    notif.Status,
+			Id:        notif.Id,
+			CreatedAt: notif.CreatedAt,
+
 		}
 		if notif.EntityTypeId == 1 || notif.EntityTypeId == 2 {
 			notification.Entity = "transaction"
 			// get the txn:
 			var transaction Transaction
-
 			err := ns.DB.Raw("SELECT T.shares, T.price, T.transaction_time, P.player_name from transactions as T join players as P on P.player_id = T.player_id  where T.id = ?", notif.EntityId).Scan(&transaction).Error
 
 			if err != nil {
